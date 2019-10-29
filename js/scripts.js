8d90ae7df9f02ad63390df56e5c728fb272ea336
Anton Sharykin and Uriel Gonzalez <uriel@>
2019-10-29 15:04:27 -0700

    Add for loop functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,33 @@
+// $(document).ready(function() {
+//   var summandsArray = 0;
+//   var summands = [1, 2, 3, 4, 5];
+//   for (var index = 0; index < summands.length; index += 1) {
+//     summandsArray += summands[index];
+//   }
+//   alert(summandsArray);
+//
+// });
+
+// $(document).ready(function() {
+//   var num = 0;
+//   while(num < 30){
+//   num = num + 5;
+//   // var newArray = num.split();
+//   $(".results").append(num);
+//   // $(".results").show();
+//   }
+// });
+
 $(document).ready(function() {
+  // for(starting point, end point, increments){
+  //   do things, for each increment.
+  // }
 
 
-
+  var n1 = 5
+  var n2 = 30
+  var result =[];
+  for  (var index=n1; index <= n2; index += n1) {
+    $("#results").append("<li>" + index + "</li>");
+  }
 });
